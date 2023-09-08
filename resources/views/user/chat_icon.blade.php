a0688ec3d88616ff8746d86f207f75c1b1e6bcad
DevTen <logiccmpl@gmail.com>
2023-09-08 11:30:29 +0100

    number changed

--- resources/views/user/chat_icon.blade.php
+++ resources/views/user/chat_icon.blade.php
@@ -1,3 +1,3 @@
-<a href="//wa.link/bei719" style="position : fixed; left : 4px; bottom : 10vh; z-index : 21212">
+<a href="//wa.link/nmocfj" style="position : fixed; left : 4px; bottom : 10vh; z-index : 21212">
     <img src="/whatsapp_chat.png" style="height: 50px" alt="">
 </a>
